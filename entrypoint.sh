a8414f05eadbdb88f36a8b9dcaa5d9f31a127a8a
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2020-01-14 11:10:25 -0300

    Let user set password as env var

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set +x
 
+if [ -n "${CYRUS_PASSWORD}" ];
+then
+    echo "cyrus:${CYRUS_PASSWORD}" | chpasswd
+fi
+
 source /etc/sysconfig/saslauthd
 /usr/sbin/saslauthd -m $SOCKETDIR -a $MECH $FLAGS
 
